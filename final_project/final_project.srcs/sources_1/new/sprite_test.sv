a746435fa0f4b8d53077814c79cc6e9ed67f2bed
Diana Hernandez <diher@DHCP-POOL-18-25-26-189.MIT.EDU>
2019-10-29 17:57:27 -0400

    testing upload from simlink

--- final_project/final_project.srcs/sources_1/new/sprite_test.sv
+++ final_project/final_project.srcs/sources_1/new/sprite_test.sv
@@ -1,26 +1,34 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/29/2019 03:36:39 PM
-// Design Name: 
-// Module Name: sprite_test
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
+module sprite_test;
 
-module sprite_test(
+   // Inputs
+   logic clk;
+   logic data_in;
 
-    );
+   // Outputs
+   logic  [7:0] data_out;
+
+   // Instantiate the Unit Under Test (UUT)
+   sample uut (
+      .clk(clk), 
+      .data_in(data_in), 
+      .data_out(data_out)
+   );
+
+   always #5 clk = !clk;
+   
+   initial begin
+      // Initialize Inputs
+      clk = 0;
+      data_in = 0;
+
+      // Wait 100 ns for global reset to finish
+      #100;
+        
+      // Add stimulus here
+
+   end
+      
 endmodule
