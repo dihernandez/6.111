fce4787553fa4ea346b9730023fa41a7fbcc757b
ray-dedhia <rdedhia@mit.edu>
2019-10-29 20:16:22 -0400

    added seven_seg_display.sv and displayed a counter that increments by 1 every second

--- final_project/files/camera_top.sv
+++ final_project/files/camera_top.sv
@@ -7,7 +7,9 @@ module camera_top_level (
         input clk_100mhz,
         input [15:0] sw,
         output logic led16_b,
-        output logic [15:0] led
+        output logic [15:0] led,
+        output ca, cb, cc, cd, ce, cf, cg, dp,  // segments a-g, dp
+        output[7:0] an    // Display location 0-7
     );
 
     // create 65mhz system clock, happens to match 1024 x 768
@@ -23,7 +25,7 @@ module camera_top_level (
     // declare inputs / outputs
     logic start_timer, counting, expired, one_hz;
     logic [3:0] value, count_out;
-    timer timer_uut( 
+    timer timer_uut ( 
         // inputs
         .clock(clk_65mhz),
         .start_timer(start_timer),
@@ -35,12 +37,29 @@ module camera_top_level (
         .count_out(count_out) 
     );
 
+    // seven segment display module
+    // declare inputs / outputs
+    logic [31:0] data_in;
+    wire [6:0] segments;
+    assign {cg, cf, ce, cd, cc, cb, ca} = segments[6:0];
+    display_8hex display_uut (
+        // inputs
+        .clk_in(clk_65mhz),
+        .data_in(data_in),
+        // outputs
+        .seg_out(segments),
+        .strobe_out(an)
+    );
+
     // test timer
     logic one_second_pulse;
     assign led16_b = one_second_pulse;
 
     always_ff @(posedge clk_65mhz) begin
-        if (one_hz) one_second_pulse <= !one_second_pulse;
+        if (one_hz) begin
+            one_second_pulse <= !one_second_pulse;
+            data_in <= data_in + 1;
+        end
     end
 
 endmodule // top_level
