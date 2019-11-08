31f986302e93f33eb1b593641725ead0bf47a2d8
ray-dedhia <rdedhia@mit.edu>
2019-11-08 17:19:15 -0500

    Update camera_top.sv

--- final_project/files/camera_top.sv
+++ final_project/files/camera_top.sv
@@ -145,16 +145,41 @@ module camera_top_level (
                           .pixel_data_out(output_pixels),
                           .pixel_valid_out(valid_pixel),
                           .frame_done_out(frame_done_out));
-   
 
     wire phsync,pvsync,pblank;
+    wire reset, up, down;
+    pong_game pg(.vclock_in(clk_65mhz),.reset_in(reset),
+                .up_in(up),.down_in(down),.pspeed_in(sw[15:12]),
+                .hcount_in(hcount),.vcount_in(vcount),
+                .hsync_in(hsync),.vsync_in(vsync),.blank_in(blank),
+                .phsync_out(phsync),.pvsync_out(pvsync),.pblank_out(pblank),.pixel_out(pixel)
+    );
+
+
+    wire border = (hcount==0 | hcount==1023 | vcount==0 | vcount==767 |
+                   hcount == 512 | vcount == 384);
 
     reg b,hs,vs;
     always_ff @(posedge clk_65mhz) begin
-        hs <= phsync;
-        vs <= pvsync;
-        b <= pblank;
-        rgb <= cam;
+      if (sw[1:0] == 2'b01) begin
+         // 1 pixel outline of visible area (white)
+         hs <= hsync;
+         vs <= vsync;
+         b <= blank;
+         rgb <= {12{border}};
+      end else if (sw[1:0] == 2'b10) begin
+         // color bars
+         hs <= hsync;
+         vs <= vsync;
+         b <= blank;
+         rgb <= {{4{hcount[8]}}, {4{hcount[7]}}, {4{hcount[6]}}} ;
+      end else begin
+         // default: pong
+         hs <= phsync;
+         vs <= pvsync;
+         b <= pblank;
+         rgb <= cam;
+      end
     end
 
     // for debugging
@@ -216,3 +241,60 @@ module camera_top_level (
     assign vga_vs = ~vs;
 
 endmodule // camera_top_level
+
+//////////////////////////////////////////////////////////////////////////////////
+// syncrhonize module
+//////////////////////////////////////////////////////////////////////////////////
+
+module synchronize #(parameter NSYNC = 3)  // number of sync flops.  must be >= 2
+                   (input clk,in,
+                    output reg out);
+
+  reg [NSYNC-2:0] sync;
+
+  always_ff @ (posedge clk)
+  begin
+    {out,sync} <= {sync[NSYNC-2:0],in};
+  end
+endmodule
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// pong_game: the game itself!
+//
+////////////////////////////////////////////////////////////////////////////////
+
+module pong_game (
+   input vclock_in,        // 65MHz clock
+   input reset_in,         // 1 to initialize module
+   input up_in,            // 1 when paddle should move up
+   input down_in,          // 1 when paddle should move down
+   input [3:0] pspeed_in,  // puck speed in pixels/tick 
+   input [10:0] hcount_in, // horizontal index of current pixel (0..1023)
+   input [9:0]  vcount_in, // vertical index of current pixel (0..767)
+   input hsync_in,         // XVGA horizontal sync signal (active low)
+   input vsync_in,         // XVGA vertical sync signal (active low)
+   input blank_in,         // XVGA blanking (1 means output black pixel)
+        
+   output phsync_out,       // pong game's horizontal sync
+   output pvsync_out,       // pong game's vertical sync
+   output pblank_out,       // pong game's blanking
+   output [11:0] pixel_out  // pong game's pixel  // r=23:16, g=15:8, b=7:0 
+   );
+
+   wire [2:0] checkerboard;
+        
+   // REPLACE ME! The code below just generates a color checkerboard
+   // using 64 pixel by 64 pixel squares.
+   
+   assign phsync_out = hsync_in;
+   assign pvsync_out = vsync_in;
+   assign pblank_out = blank_in;
+   assign checkerboard = hcount_in[8:6] + vcount_in[8:6];
+
+   // here we use three bits from hcount and vcount to generate the
+   // checkerboard
+
+   assign pixel_out = {{4{checkerboard[2]}}, {4{checkerboard[1]}}, {4{checkerboard[0]}}} ;
+     
+endmodule
